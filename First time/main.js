b5fcb92a170e3caeae469d545b570ff36d13f012
Danil <danilpecha7@gmail.com>
2020-07-22 18:25:13 +0300

    Second lesson

--- First time/main.js
+++ First time/main.js
@@ -2,15 +2,35 @@ const numberOfFilms = +prompt("Сколько фильмов вы посмотр
 
 const personalMovieDB = {
     count: numberOfFilms,
-    movies: [],
-    actors: [],
+    movies: {},
+    actors: {},
     genres: [],
     privat: false
 };
 
-const LastFilm = prompt("Один из последних просмотренных фильмов?", "");
-const Rate = +prompt("На сколько оцените?", "");
+for (let i = 0; i<numberOfFilms; i++){
+    var Film = prompt("Какой фильм вы смотрели?", "");
+    if (Film === "" || Film === null || Film.length > 50){
+        console.log("Четко");
+        alert("Названия нет! Попробуйте еще раз.");
+        i--;
+        continue;
+    }
+    const Rate = prompt("Как вы его оцениваете?", "");
+    personalMovieDB.movies[Film] = Rate;
+}
 
-personalMovieDB.movies[LastFilm] = Rate;
+if (personalMovieDB.count<10){
+    alert("Мало");
+} 
+else if (personalMovieDB.count>=10 && personalMovieDB.count <= 30) {
+    alert("Norm");
+}
+else if (personalMovieDB.count >= 30){
+    alert("Mnoga");
+}
+else {
+    alert("Error");
+}
 
 console.log(personalMovieDB);
